efe8af18b4c33f180e818bcc6fb4b53e06acab56
Demilade Soremekun <demiladesoremekun@gmail.com>
2020-04-12 16:44:27 +0100

    Project updates

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -32,7 +32,7 @@ class CoursesController extends Controller
     
     static function getNextCourseCode(CdtLocation $location, $start_date) {
         $courses = CdtCourse::whereIn('location_id', CdtLocation::where('region_id', $location->region->id)->pluck('id')->toArray())->where('start_date', 'like', substr($start_date, 0, 4).'-%');
-        dd($courses);
+        dd($courses->count());
         if ($courses->count() == 0) {
             $new_code = '001';
         } else {
